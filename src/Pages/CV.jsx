e8c285ee10ad0666464ddf48b9d69017accd5031
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2026-02-18 12:23:11 +0000

    ajout .pdf bouton download cv

--- src/Pages/CV.jsx
+++ src/Pages/CV.jsx
@@ -50,7 +50,7 @@ const CV = () => {
         </div>
 
         <div className="mx-auto mb-8 w-max rounded-sm bg-[#214b9b] px-2 py-3 transition hover:scale-105 active:scale-100 max-lg:mb-5">
-          <a href="/CV-JaafarBenda" download>
+          <a href="/CV-JaafarBenda.pdf" download>
             <FaFileDownload size={28} color="white" />
           </a>
         </div>
